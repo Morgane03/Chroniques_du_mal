77a7cb2f8869c5075572f4a24d453b00bfe0afb4
m v <morgane.voisin@e-artsup.net>
2023-04-12 12:37:01 +0200

    Debut Dialogue

---
+++ Assets/Script/Dialogue/DialogueTrigger.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour
+{
+    [Header("Visual Cue")]
+    [SerializeField] private GameObject visualCue;
+
+    [Header("Ink JSON")]
+    [SerializeField] TextAsset InkJSON;
+
+    private bool playerInRange;
+
+    private void Awake()
+    {
+        
+        visualCue.SetActive(false);
+    }
+
+    private void Start()
+    {
+        playerInRange = true;
+    }
+
+    private void Update()
+    {
+        if(playerInRange)
+        {
+            visualCue.SetActive(true);
+            if(Input.GetKeyDown(KeyCode.Space))
+            {
+                Debug.Log(InkJSON.text);
+            }
+        }
+    }
+
+
+
+}
